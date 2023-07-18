1298d22e30ec3efb522c7c4b63d68b4c62b7935f
ayemoy <ayelet431@gmail.com>
2023-07-18 20:13:35 +0300

    1

--- src/components/RecipePreviewList.vue
+++ src/components/RecipePreviewList.vue
@@ -37,11 +37,11 @@ export default {
     async updateRecipes() {
       try {
         const response = await this.axios.get(
-          this.$root.store.server_domain + "/recipes/random",
+          this.$root.store.server_domain + "/recipes/random3recipes",
           // "https://test-for-3-2.herokuapp.com/recipes/random"
         );
 
-        // console.log(response);
+        console.log(response);
         const recipes = response.data.recipes;
         this.recipes = [];
         this.recipes.push(...recipes);
