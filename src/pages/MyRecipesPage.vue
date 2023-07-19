e21369d6267a4f0d7c96ee922456f2dc02e7bb0a
ayemoy <ayelet431@gmail.com>
2023-07-19 19:41:06 +0300

    222

--- src/pages/MyRecipesPage.vue
+++ src/pages/MyRecipesPage.vue
@@ -1,5 +1,33 @@
 
+<template>
+  <div class="myrecipes-container">
+    <RecipePreviewList ref="myrecipes" title="My Recipes" route_name="/users/myRecipes" class="MyRecipes"/>
+  </div>
+</template>
+  
+<script>
+import RecipePreviewList from '../components/RecipePreviewList.vue'
+export default {
+  name:"MyRecipes",
+  components: {
+      RecipePreviewList,
+  },
+  mounted() {
+    this.$refs.myRecipes.updateMyRecipes();
+  },
+}
+</script>
+  
+<style>
+.myrecipes-container {
+  display: flex;  
+  align-items: center;
+  justify-content: center;
+  padding-bottom: 30px;
+}
+</style>
 
+<!-- 
 <template>
     <div class="container">
       <br/>
@@ -35,4 +63,4 @@
     font-size: 30px;
   }
   </style>
-  
+   -->
