93f359be8bd7236a752a0cd0f9731fee930ba510
ayemoy <ayelet431@gmail.com>
2023-07-22 22:25:54 +0300

    Nvbar fixed

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -10,21 +10,21 @@
         <router-link tag="b-navbar-brand" :to="{name:'main'}" style="font-weight: 1000; color: rgb(215, 141, 52)">Foodie</router-link>
         
         <!-- Home -->
-        <b-navbar-nav>
+        <b-navbar-nav id="navnav">
           <router-link :to="{name:'main'}"><b style="color: rgb(0, 0, 0)">Home</b></router-link>
         </b-navbar-nav>
         <!-- Search -->
-        <b-navbar-nav>
+        <b-navbar-nav id="navnav">
           <router-link :to="{name:'search'}"><b style="color: rgb(0, 0, 0)">Search</b></router-link >
         </b-navbar-nav>
         <!-- About -->
-        <b-navbar-nav>
+        <b-navbar-nav id="navnav">
           <router-link to="/AboutUs"><b style="color: rgb(0, 0, 0)">About</b></router-link>
         </b-navbar-nav>
         <!-- Registred user  -->
         <b-navbar-nav v-if="$root.store.username">
           <!-- Create Recipe Modal -->
-          <b-navbar-nav>
+          <b-navbar-nav id="navnav">
           <b-nav-item v-b-modal.modal-1 id="modal-1" @click="showModal"
             ><b style="color: rgb(0, 0, 0)">Create Recipe</b></b-nav-item>
             <CreateRecipePage v-if="createRecipeClicked"></CreateRecipePage>
@@ -47,7 +47,7 @@
             <b-nav-item>
               <b-row> 
                 <b-col>
-                  <b-navbar-item><h3 style="color:rgb(0, 0, 0) ;">Hello {{$root.store.username}}</h3></b-navbar-item>  
+                  <b-navbar-item><h3 style="color:rgb(0, 0, 0) ;">Hello {{$root.store.username}}!</h3></b-navbar-item>  
                 </b-col> 
                 <router-link v-b-modal.modal-1 id="modal-1" to="/" @click.native="Logout()" ><b style="color:rgb(255, 0, 0) ;">Logout</b></router-link>
               </b-row>
@@ -100,6 +100,10 @@
     height:50px ;
     color: white;
   }
+   
+
+  
+
   .bold-option .dropdown-toggle {
     color: rgb(0, 0, 0) !important;
   }
@@ -117,6 +121,10 @@
     zoom: 7%;
     margin-left: -16vw;
     margin-right: 6vw;
+  }
+  #navnav{
+    padding-left: 1.5vw;
+    padding-right: 1.5vw;
   }
 
   </style>
